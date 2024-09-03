8cf8019dc24dca624d6ef4c20dc2fa362d065bb7
vllesol18 <paul.llesol@awsys-i.com>
2024-09-03 14:24:48 +0800

    added JavaActivity1.java

---
+++ JavaActivity1.java
@@ -0,0 +1,19 @@
+package JavaActivities;
+
+public class JavaActivity1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "Sol";
+		int age = 23;
+		String country = "Philippines";
+		String sport = "Chess";
+		float hours = 3.5f;
+		String game = "Valorant";
+		String subject = "Science";
+		int grade = 75;
+		
+		System.out.println("My name is " + name + ".\nI'm "+ age + " years old and from " + country + ".\nMy favorite sport is "+ sport + ".\nI play for "+ hours + "hours a day.\nI like to play" + game + " when I'm tired.\nIn school, my favorite subject was " + subject + ".\nI scored a " + grade);
+	}
+
+}
